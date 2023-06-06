edcddc8ce74003b437e53347e85fc8464833057a
wdendugu <wdendugu125@gmail.com>
2023-06-06 19:53:06 -0300

    localStorage added

--- components/CartContext.js
+++ components/CartContext.js
@@ -1,34 +1,38 @@
-import { createContext } from "react"
-import { useState } from "react"
+import {createContext, useEffect, useState} from "react";
 
-export const CartContext = createContext({})
+export const CartContext = createContext({});
 
-export function CartContextProvider ({children}) {
-    const [cartProducts,setCartProducts] = useState([])
-
-    function addProduct(productId){
-        setCartProducts(prev => [...prev, productId])
+export function CartContextProvider({children}) {
+  const ls = typeof window !== "undefined" ? window.localStorage : null;
+  const [cartProducts,setCartProducts] = useState([]);
+  useEffect(() => {
+    if (cartProducts?.length > 0) {
+      ls?.setItem('cart', JSON.stringify(cartProducts));
     }
-
-    function removeProduct(productId){
-        setCartProducts(prev => {
-            const pos = prev.indexOf(productId)
-            if (pos !== -1) {
-                return prev.filter((value,index) => index !== pos)
-            }
-            return prev
-        })
+  }, [cartProducts]);
+  useEffect(() => {
+    if (ls && ls.getItem('cart')) {
+      setCartProducts(JSON.parse(ls.getItem('cart')));
     }
-    
-    return (
-        <CartContext.Provider 
-            value={{
-                cartProducts, 
-                setCartProducts, 
-                addProduct, 
-                removeProduct
-            }}
-        >{children}
-        </CartContext.Provider>
-    )
+  }, []);
+  function addProduct(productId) {
+    setCartProducts(prev => [...prev,productId]);
+  }
+  function removeProduct(productId) {
+    setCartProducts(prev => {
+      const pos = prev.indexOf(productId);
+      if (pos !== -1) {
+        return prev.filter((value,index) => index !== pos);
+      }
+      return prev;
+    });
+  }
+  function clearCart() {
+    setCartProducts([]);
+  }
+  return (
+    <CartContext.Provider value={{cartProducts,setCartProducts,addProduct,removeProduct,clearCart}}>
+      {children}
+    </CartContext.Provider>
+  );
 }
